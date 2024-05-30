609738499ded490f69f1a05e386cba59dfe1341f
Pavel2812 <pavel0837mi@gmail.com>
2024-05-30 13:13:05 +0300

    Refactor: Remove unnecessary comments and docstrings

--- Homework7.py
+++ Homework7.py
@@ -1,25 +1,19 @@
-"""
-Homework_7
-"""
+"""Модуль для домашнего задания 7."""
+
 # pylint: disable=invalid-name
 # Быки и коровы
-# Генерируем случайное число с неповторяющимися цифрами
-# (Делаем это только один раз в начале)
 import random
 while True:
     random_number = random.randint(1000, 9999)
-    # Проверяем, что все 4 цифры уникальны
     if len(str(random_number)) == len(set(str(random_number))):
-        print(random_number)   # Выводим число для отладки (можно убрать)
+        print(random_number)
         break
 print("Загадано")
 
-# Начинаем цикл угадывания (загаданное число при этом не меняется)
 while True:
     number = input("Введите ваше число: ")
     print("Вы ввели:", number)
 
-    # Проверяем наличие повторяющихся цифр
     if len(set(number)) != len(number):
         print("Число должно содержать только уникальные цифры.")
         continue
@@ -27,13 +21,11 @@ while True:
     cows = 0
     bulls = 0
 
-    # Проверяем на 'коров' (совпадение цифр)
     for i in range(len(str(number))):
         for j in range(len(str(random_number))):
             if i != j and str(number)[i] == str(random_number)[j]:
                 cows += 1
 
-    # Проверяем на 'быков' (совпадение цифр и их позиций)
     for i in range(len(str(number))):
         for j in range(len(str(random_number))):
             if i == j and str(number)[i] == str(random_number)[j]:
@@ -44,7 +36,7 @@ while True:
 
     if bulls == 4:
         print("Вы выиграли!")
-        break  # Прерываем цикл, если угадали
+        break
 
 # Пирамида
 N = 10
