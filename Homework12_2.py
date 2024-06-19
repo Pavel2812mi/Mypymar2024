c03f352186fd2f9fb351f08616911270c74196ff
Pavel2812 <pavel0837mi@gmail.com>
2024-06-19 17:57:54 +0300

    Refactor: Remove duplicate code from Homework11_2 to fix Pylint error R0801.

--- Homework12_2.py
+++ Homework12_2.py
@@ -1,16 +1,6 @@
 """Конвертер валют"""
 
 
-class Deposit:
-    """A class to represent all created deposits"""
-    def __init__(self, name, initial_deposit_amount,
-                 deposit_term, percentage_per_annum):
-        self.name = name
-        self.initial_deposit_amount = initial_deposit_amount
-        self.deposit_term = deposit_term
-        self.percentage_per_annum = percentage_per_annum
-
-
 class Currency:
     """A class to represent all currencies and their rates"""
     def __init__(self, currency_name, USD_exchange_rate,
@@ -60,37 +50,8 @@ class Bank:
                 f"You received {result_amount:.2f} "
                 f"{target_currency_obj.currency_name}")
 
-    def create_deposit(self, deposit_name, initial_deposit_amount,
-                       deposit_term, percentage_per_annum):
-        """Method for deposit creation"""
-        if self.is_registred is True:
-            deposit1 = Deposit(deposit_name, initial_deposit_amount,
-                               deposit_term, percentage_per_annum)
-            self.deposits[deposit_name] = deposit1
-            print(f"You have successfully created "
-                  f"a deposit {deposit_name}")
-        else:
-            print("Sorry, you need to be a client of the bank "
-                  "to create a deposit")
-
-    def withdraw_deposit(self, deposit_name):
-        """Method for deposit withdraw"""
-        deposit = self.deposits.get(deposit_name)
-        if deposit:
-            monthly_interest = deposit.percentage_per_annum / 12 / 100
-            months = deposit.deposit_term * 12
-            end_deposit_amount = (deposit.initial_deposit_amount
-                                  * (1 + monthly_interest) ** months)
-            return (f"Итоговая сумма депозита"
-                    f" {deposit_name}: {end_deposit_amount:.2f}")
-        return f"Депозит с именем {deposit_name} не найден."
-
 
 client1 = Bank("Pavel_Danilov", True)
-client1.create_deposit("Deposit_1", 1000, 1, 10)
-assert (client1.withdraw_deposit("Deposit_1")
-        == "Итоговая сумма депозита Deposit_1: 1104.71"), \
-    "Incorrect final deposit amount calculation"
 assert ((client1.exchange_currency("USD", 10, "EUR"))
         == "Currency exchange succesful. You received 9.29 EUR"), \
     "Incorrect Currency exchange calculation"
