ce1f86c92852283e3bf5c55047c42f329e73b648
Pavel2812 <pavel0837mi@gmail.com>
2024-07-27 21:48:28 +0300

    Add: unique selectors for homework23 assignment

---
+++ homework23/1_Create_new_account.py
@@ -0,0 +1,14 @@
+# Create new account:
+
+# 1) https://thinking-tester-contact-list.herokuapp.com/
+
+# //button[@id='signup']
+
+# 2) https://thinking-tester-contact-list.herokuapp.com/addUser
+
+# //input[@id='firstName']
+# //input[@id='lastName']
+# //input[@id='email']
+# //input[@id='password']
+
+# //button[@id='submit']
---
+++ homework23/3_Update_existing_contact.py
@@ -0,0 +1,34 @@
+# Update existing contact:
+
+# 1) https://thinking-tester-contact-list.herokuapp.com/
+
+# //input[@id='email']
+# //input[@id='password']
+
+# //button[@id='submit']
+
+
+# 2) https://thinking-tester-contact-list.herokuapp.com/contactList
+
+#  .contactTableBodyRow td:not([hidden])
+
+
+# 3) https://thinking-tester-contact-list.herokuapp.com/contactDetails
+
+# //button[@id='delete']
+
+
+# 4) https://thinking-tester-contact-list.herokuapp.com/editContact
+
+# //input[@id='firstName']
+# //input[@id='lastName']
+# //input[@id='birthdate']
+# //input[@id='phone']
+# //input[@id='street1']
+# //input[@id='street2']
+# //input[@id='city']
+# //input[@id='stateProvince']
+# //input[@id='postalCode']
+# //input[@id='country']
+
+# //button[@id='submit']
